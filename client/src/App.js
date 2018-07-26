320b6bfb8fe709620f0333a699300884ad8d60ed
helioboros <felinewraith@gmail.com>
2018-07-26 09:19:26 -0400

    save auth tokens

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom'
 import SignUpLogin from './components/SignUpLogin'
 import axios from 'axios'
+import {saveAuthTokens, setAxiosDefaults, userIsLoggedIn} from "./util/SessionHeaderUtil"
 
 class App extends Component {
 
@@ -16,26 +17,55 @@ class App extends Component {
                 password: password,
                 password_confirmation: password_confirmation
             }
-            await axios.post('/auth', payload)
-
-            this.setState({signedIn: true})
-
+            const response = await axios.post('/auth', payload)
+            saveAuthTokens(response.headers)
+    
+            this.setState({
+                signedIn: true,
+            })
+    
         } catch (error) {
             console.log(error)
         }
     }
-
+    
     signIn = async (email, password) => {
         try {
             const payload = {
                 email,
                 password
             }
-            await axios.post('/auth/sign_in', payload)
-
-            this.setState({signedIn: true})
-
+            const response = await axios.post('/auth/sign_in', payload)
+            saveAuthTokens(response.headers)
+            
+    
+            const posts = await this.getPosts()
+    
+            this.setState({
+                signedIn: true,
+                posts
+            })
+    
         } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async componentWillMount() {
+        try {
+            const signedIn = userIsLoggedIn()
+    
+            let posts = []
+            if (signedIn) {
+                setAxiosDefaults()
+                posts = await this.getPosts()
+            }
+    
+            this.setState({
+                posts,
+                signedIn,
+            })
+        } catch(error) {
             console.log(error)
         }
     }
